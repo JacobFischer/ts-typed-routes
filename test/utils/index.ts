038e21749c0a16ad9a8287b19d0f381c89647bc9
Jacob Fischer <jacob.t.fischer@gmail.com>
2019-05-21 06:28:44 -0500

    Move express specific test util functions to their own file

--- test/utils/index.ts
+++ test/utils/index.ts
@@ -1,7 +1,4 @@
-import express from "express";
 import { stat } from "fs-extra";
-import { Server } from "http";
-import { createServer } from "net";
 import { join } from "path";
 
 const CLIENT_DIST_DIR = join(__dirname, "../../dist/client/");
@@ -12,36 +9,4 @@ export async function getClientDistDir() {
     return CLIENT_DIST_DIR;
 }
 
-export const closeServer = (server: Server) => new Promise<void>((resolve, reject) => {
-  server.close((err) => {
-      if (err) {
-          reject(err);
-      } else {
-          resolve();
-      }
-  });
-});
-
-export const newExpressServer = (
-    port: number,
-    withApp?: (app: express.Application) => void | Promise<unknown>,
-) => new Promise<Server>(async (resolve) => {
-    const app = express();
-    if (withApp) {
-        await withApp(app);
-    }
-    const server = app.listen(port, () => resolve(server));
-});
-
-export const isPortTaken = (port: number) => new Promise<boolean>((resolve, reject) => {
-  const tester = createServer();
-  tester.once("error", (err: Error & { code?: string }) => {
-      if (err.code === "EADDRINUSE") {
-          resolve(true); // address was in use, port is thus taken
-      } else {
-          reject(err); // unexpected other error occurred
-      }
-  });
-  tester.once("listening", () => tester.close(() => resolve(false)));
-  tester.listen(port);
-});
+export * from "./express";
