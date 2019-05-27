d4d406efdd474a2bcdd2075597412b68baa1f322
Jacob Fischer <jacob.t.fischer@gmail.com>
2019-05-26 21:57:32 -0500

    Move sever build to Webpack to preempt servre side css loading

---
+++ src/server/webpack.config.ts
@@ -0,0 +1,47 @@
+/* eslint-env node */
+import { resolve } from "path";
+import * as webpack from "webpack";
+import nodeExternals from "webpack-node-externals";
+
+export default (
+    env: undefined,
+    options: webpack.Configuration,
+): webpack.Configuration => ({
+    devtool: options.mode === "development"
+        ? "source-map"
+        : false,
+    entry: [
+        resolve(__dirname, "./index.tsx"),
+    ],
+    externals: [nodeExternals()], // we don't want to bundle node_modules (external modules)
+    module: {
+        rules: [
+            {
+                exclude: /node_modules/,
+                test: /\.tsx?$/,
+                use: [
+                    {
+                        loader: "ts-loader",
+                    },
+                ],
+            },
+        ],
+    },
+    node: { // we will be running on node, no polyfills or mocks needs; so assume all node built-ins are safe to use
+        console: false,
+        process: false,
+        global: false,
+        __filename: false,
+        __dirname: false,
+        Buffer: false,
+        setImmediate: false,
+    },
+    output: {
+        filename: "[name].js",
+        path: resolve(__dirname, "../../dist/server"),
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    target: "node",
+});
