1bdd7f3345596f48d1c6a97a8e1313321e1c0b3b
Jacob Fischer <jacob.t.fischer@gmail.com>
2019-06-12 21:36:50 -0500

    Update static site building with tests

---
+++ test/static/static.test.ts
@@ -0,0 +1,53 @@
+import { stat, remove, ensureDir, emptyDir, readdir } from "fs-extra";
+import { join, resolve } from "path";
+import { buildStaticPages } from "../../src/static/build";
+import { routes } from "../../src/shared/routes";
+
+const TEMP_DIR = resolve(__dirname, "../../temp/");
+const OUTPUT_DIR = join(TEMP_DIR, "static/");
+
+beforeAll(() => ensureDir(OUTPUT_DIR));
+afterEach(() => emptyDir(OUTPUT_DIR));
+afterAll(async () => {
+    await remove(OUTPUT_DIR);
+    await remove(TEMP_DIR);
+});
+
+describe("Static site generation", () => {
+    it("Should be a function", () => {
+        expect(typeof buildStaticPages).toBe("function");
+    });
+
+    it("Should generate a static site", async () => {
+        const preStat = await stat(OUTPUT_DIR);
+        expect(preStat.isDirectory()).toBe(true);
+        const preFiles = await readdir(OUTPUT_DIR);
+        expect(preFiles.length).toBe(0);
+
+        await buildStaticPages(OUTPUT_DIR);
+
+        const postStat = await stat(OUTPUT_DIR);
+        expect(postStat.isDirectory()).toBe(true);
+        const postFiles = await readdir(OUTPUT_DIR);
+        expect(postFiles.length).toBeGreaterThanOrEqual(2); // 404 and index pages must exist
+        expect(postFiles).toContain("404.html");
+
+        await Promise.all(routes.map(async ([route]) => {
+            const staticPath = join(OUTPUT_DIR, route, "index.html");
+            const fileStats = await stat(staticPath);
+            expect(fileStats.isFile()).toBe(true);
+        }));
+    }, 12500);
+
+    it("Use a custom logger", async () => {
+        let logged = false;
+        await buildStaticPages(OUTPUT_DIR, (...strings) => {
+            logged = true;
+            for (const str of strings) {
+                expect(typeof str).toBe("string");
+            }
+        });
+
+        expect(logged).toBe(true);
+    }, 12500);
+});
